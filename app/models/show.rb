96b4b7de8e451a282ab7b65e303b63873f26e64c
Kiri Um <kirirotha@gmail.com>
2020-07-26 13:29:14 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
 
   def self.most_popular_show
     x = highest_rating
-    Show.all.where("rating == ?", x).limit1
+    Show.all.where("rating == ?", x).first
   end
 
 
